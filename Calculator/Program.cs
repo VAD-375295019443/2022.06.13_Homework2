2741c06689a13d4caa3fb1c35ee5f19ec4bfbac7
VAD-375295019443 <105417922+VAD-375295019443@users.noreply.github.com>
2022-06-08 16:52:27 +0300

    Changes

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -92,7 +92,7 @@ namespace Calculator
                     continue;
                 }
                 */
-                
+
                 //------------------------------------------------------------------------------
                 //------------------------------------------------------------------------------
                 //------------------------------------------------------------------------------
@@ -117,14 +117,14 @@ namespace Calculator
                     else if (strExpression[0] == '-')
                     {
                         strExpression = strExpression.Remove(0, 1);
-                        strExpression =  strExpression.Insert(0, "(-1)*");
+                        strExpression = strExpression.Insert(0, "(-1)*");
                     }
                 }
 
 
                 F_voiFunctionControlFuture(ref strExpression); //Создаем функции /, *, +,-.
 
-                
+
                 bool booControlCalculationOfSimpleFunctions = true;
                 while (booControlCalculationOfSimpleFunctions == true)
                 {
@@ -154,7 +154,7 @@ namespace Calculator
                 Console.WriteLine(strExpression);
                 Console.WriteLine("");
 
-                
+
                 /*
                 string xx = Console.ReadLine();
                 bool z = false;
@@ -170,114 +170,6 @@ namespace Calculator
 
 
 
-        static bool F_booCalculationOfComplexFunctions(ref string f_strExpression)
-        {
-            string strExpression = f_strExpression;
-
-            int intStartBracket = 0;
-            int intStopBracket = 0;
-
-            string? strFunctionName = null;
-            string? strFunctionBody = null;
-            bool booControlFunctionName = false;
-            bool booControlFunctionBody = false;
-
-            int intCountBracket = 0;
-
-
-
-            /*
-            bool booResult = false;
-            string strNumber = null;
-            double dblNumber = 0;
-            string strTransformedFunction = null;
-            */
-
-
-
-
-
-            for (int int1 = 0; int1 <= strExpression.Length - 1; int1++)
-            {
-                strFunctionName = null;
-                booControlFunctionName = false;
-
-                strFunctionBody = null;
-                booControlFunctionBody = false;
-
-                if (strExpression[int1] == '[') //Блок поиска [] скобок.
-                {
-                    intStartBracket = int1;
-
-                    for (int int2 = int1+1; int2 <= strExpression.Length - 1; int2++)
-                    {
-                        if (strExpression[int2] == ']')
-                        {
-                            intStopBracket = int2;
-                            strFunctionName = strExpression.Substring(intStartBracket + 1, intStopBracket - intStartBracket - 1); //Врезаем имя функции. Берем кусок текста без скобок.
-                            booControlFunctionName = true;
-                            break;
-                        }
-                    }
-
-                    for (int int2 = int1+1; int2 <= strExpression.Length - 1; int2++)
-                    {
-                        if (strExpression[int2] == '{')
-                        {
-                            intStartBracket = int2;
-
-                            for (int int3 = int2+1; int3 <= strExpression.Length - 1; int3++)
-                            {
-                                if (strExpression[int3] == '}')
-                                {
-                                    intStopBracket = int3;
-                                    strFunctionBody = strExpression.Substring(intStartBracket + 1, intStopBracket - intStartBracket - 1); //Врезаем имя функции. Берем кусок текста без скобок.
-                                    booControlFunctionBody = true;
-                                    break;
-                                }
-                                else if (strExpression[int3] == '{')
-                                {
-                                    break;
-                                }
-                            }
-                            break;
-                        }
-                    }
-                }
-
-
-                ///////Обработка имени и тела функции.
-
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                if (booControlFunctionName = true && booControlFunctionBody == true)
-                {
-                    Console.WriteLine(strFunctionName);
-                    Console.WriteLine(strFunctionBody);
-                }
-
-
-
-
-            }
-
-            if (f_strExpression != strExpression)
-            {
-                f_strExpression = strExpression;
-                return (true);
-            }
-            else
-            {
-                return (false);
-            }
-        }
 
 
 
@@ -368,14 +260,14 @@ namespace Calculator
                     if (booControlNameFunction == true) //Если мы в тексте.
                     {
                         booControlNameFunction = false; //Выходим из текста.
-                        
+
                         if (f_strExpression[int1] == '(')
                         {
                             bool booResult = false;
                             int intStopBracket = 0;
 
                             booResult = F_booRightBracketPosition(f_strExpression, int1, ref intStopBracket);
-                            
+
                             if (booResult == true)
                             {
                                 f_strExpression = f_strExpression.Remove(intStopBracket, 1);
@@ -414,7 +306,7 @@ namespace Calculator
             {
                 if (f_strExpression[int1] == '/')
                 {
-                    if (f_strExpression[int1-1] == ')' && f_strExpression[int1+1] == '(')
+                    if (f_strExpression[int1 - 1] == ')' && f_strExpression[int1 + 1] == '(')
                     {
                         f_strExpression = f_strExpression.Remove(int1, 1);
                         f_strExpression = f_strExpression.Insert(int1, ".");
@@ -575,8 +467,8 @@ namespace Calculator
             }
             return (false); //Метод закончен с ошибками.
         }
-        
-        
+
+
         static bool F_booCalculationOfSimpleFunctions(ref string f_strExpression)
         {
             string strExpression = f_strExpression;
@@ -586,7 +478,7 @@ namespace Calculator
             int intStopBracket = 0;
             string strNumber = null;
             double dblNumber = 0;
-            string strTransformedFunction =  null;
+            string strTransformedFunction = null;
 
             for (int int1 = 0; int1 <= strExpression.Length - 1; int1++)
             {
@@ -600,7 +492,7 @@ namespace Calculator
                     if (booResult == true)
                     {
                         strNumber = strExpression.Substring(intStartBracket + 1, intStopBracket - intStartBracket - 1); //Берем кусок текста без скобок.
-                        
+
                         booResult = double.TryParse(strNumber, out dblNumber);
                         if (booResult == true)
                         {
@@ -623,6 +515,184 @@ namespace Calculator
             }
         }
 
+
+        static bool F_booCalculationOfComplexFunctions(ref string f_strExpression)
+        {
+            string strExpression = f_strExpression;
+
+            int intStartBracket = 0;
+            int intStopBracket = 0;
+
+            string? strFunctionName = null;
+            string? strFunctionBody = null;
+            bool booControlFunctionName = false;
+            bool booControlFunctionBody = false;
+
+            for (int int1 = 0; int1 <= strExpression.Length - 1; int1++)
+            {
+                strFunctionName = null;
+                booControlFunctionName = false;
+
+                strFunctionBody = null;
+                booControlFunctionBody = false;
+
+                if (strExpression[int1] == '[') //Блок поиска [] скобок.
+                {
+                    intStartBracket = int1;
+
+                    for (int int2 = int1 + 1; int2 <= strExpression.Length - 1; int2++)
+                    {
+                        if (strExpression[int2] == ']')
+                        {
+                            intStopBracket = int2;
+                            strFunctionName = strExpression.Substring(intStartBracket + 1, intStopBracket - intStartBracket - 1); //Врезаем имя функции. Берем кусок текста без скобок.
+                            booControlFunctionName = true;
+                            break;
+                        }
+                    }
+
+                    for (int int2 = int1 + 1; int2 <= strExpression.Length - 1; int2++)
+                    {
+                        if (strExpression[int2] == '{')
+                        {
+                            intStartBracket = int2;
+
+                            for (int int3 = int2 + 1; int3 <= strExpression.Length - 1; int3++)
+                            {
+                                if (strExpression[int3] == '}')
+                                {
+                                    intStopBracket = int3;
+                                    strFunctionBody = strExpression.Substring(intStartBracket + 1, intStopBracket - intStartBracket - 1); //Врезаем имя функции. Берем кусок текста без скобок.
+                                    booControlFunctionBody = true;
+                                    break;
+                                }
+                                else if (strExpression[int3] == '{')
+                                {
+                                    break;
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }
+
+
+
+                if (booControlFunctionName = true && booControlFunctionBody == true)
+                {
+
+
+
+                }
+
+
+
+
+
+
+                ///////Обработка имени и тела функции.
+
+
+
+
+                if (booControlFunctionName = true && booControlFunctionBody == true)
+                {
+                    Console.WriteLine(strFunctionName);
+                    Console.WriteLine(strFunctionBody);
+                }
+            }
+
+            if (f_strExpression != strExpression)
+            {
+                f_strExpression = strExpression;
+                return (true);
+            }
+            else
+            {
+                return (false);
+            }
+        }
+
+
+
+
+
+
+        static bool F_booCalculationFunction(in string f_strFunctionName, in string f_strFunctionBody, ref string f_strFunction)
+        {
+            if (f_strFunctionName == "Addition")
+            {
+
+            }
+            else if (f_strFunctionName == "Subtraction")
+            {
+
+            }
+            else if (f_strFunctionName == "Division")
+            {
+
+
+
+
+
+
+            }
+            else if (f_strFunctionName == "Multiplication")
+            {
+
+
+
+
+
+
+
+                /*
+                +
+
+                -
+
+                *
+
+                /
+
+                Abs(double value): возвращает абсолютное значение для аргумента value
+
+                Acos(double value): возвращает арккосинус value.Параметр value должен иметь значение от -1 до 1
+
+                Asin(double value): возвращает арксинус value.Параметр value должен иметь значение от -1 до 1
+
+                Atan(double value): возвращает арктангенс value
+
+                Cos(double d): возвращает косинус угла d
+
+                Cosh(double d): возвращает гиперболический косинус угла d
+
+                Exp(double d): возвращает основание натурального логарифма, возведенное в степень d
+
+                Log(double d): возвращает натуральный логарифм числа d
+
+                Log(double a, double newBase): возвращает логарифм числа a по основанию newBase
+
+                Log10(double d): возвращает десятичный логарифм числа d
+
+                Pow(double a, double b): возвращает число a, возведенное в степень b
+
+                Round(double d): возвращает число d, округленное до ближайшего целого числа
+
+                Sin(double value): возвращает синус угла value
+
+                Sqrt(double value): возвращает квадратный корень числа value
+
+                Tan(double value): возвращает тангенс угла value
+
+                !(int value): возвращает факториал числа value
+                */
+
+
+            }
+            
+            return (true);
+        }
 
 
 
